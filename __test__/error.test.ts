f4d2b7b181e459c9b2b5d45e2eb3e9fb8665f1f0
kaonmir <thswpvm@cau.ac.kr>
2021-08-05 07:49:58 +0900

    refactoring

--- __test__/error.test.ts
+++ __test__/error.test.ts
@@ -1,7 +1,5 @@
 import Chess from "../src/services/Chess";
-import { EMPTY_PIECE, Piece } from "../src/services/Piece";
-import PTYPE from "../src/services/PTYPE";
-import SIDE from "../src/services/SIDE";
+import { EMPTY_PIECE, Piece } from "../src/services/types/Piece";
 
 test("sadf", () => {
   expect(0).toBe(0);
--- __test__/log.test.ts
+++ __test__/log.test.ts
@@ -1,5 +1,5 @@
 import Chess from "../src/services/Chess";
-import { EMPTY_PIECE, Piece } from "../src/services/Piece";
+import { Piece } from "../src/services/types/Piece";
 
 test("sadf", () => {
   expect(0).toBe(0);
